72476291e3315cc4c9b54e8042da43c3ee36fa95
Mohsen <mohsen@velentir.com>
2025-06-07 12:58:46 -1000

    Adding basic scanner partial tests

--- version.zig
+++ version.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 pub const version: std.SemanticVersion = .{
     .major = 0,
-    .minor = 2,
+    .minor = 3,
     .patch = 0,
 };
--- zigma.zig
+++ zigma.zig
@@ -18,6 +18,98 @@ const builtin = @import("builtin");
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const VER = @import("version.zig").version;
+
+const Token = struct { start: u16, end: u16, value: ?f64 };
+
+const ScanError = error{UnexpectedCharacter};
+
+fn isAlpha(c: u8) bool {
+    return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or (c == '_');
+}
+
+fn isNum(c: u8) bool {
+    return c >= '0' and c <= '9';
+}
+
+fn isSpace(c: u8) bool {
+    return c == ' ' or c == '\n' or c == '\r';
+}
+
+fn isPunctuator(c: u8) bool {
+    return c == ':' or c == '=';
+}
+
+// scan a line of source into tokens
+// assumes source already broken down into lines and we scan one
+// line at a time
+fn scan(source: []const u8, allocator: std.mem.Allocator) !std.ArrayList(Token) {
+    var tokens: std.ArrayList(Token) = std.ArrayList(Token).init(allocator);
+    // empty line
+    if (source.len == 0) {
+        return tokens;
+    }
+
+    // scan the line
+    var i: u16 = 0;
+
+    while (i < source.len) : (i += 1) {
+        var c: u8 = source[i];
+
+        // skip spaces
+        if (isSpace(c)) {
+            continue;
+        }
+
+        // punctuators
+        if (isPunctuator(c)) {
+            try tokens.append(Token{ .start = i, .end = i + 1, .value = null });
+            continue;
+        }
+
+        // handle words
+        if (isAlpha(c)) {
+            const start = i;
+            c = source[i];
+            while (i < source.len and (isAlpha(c) or isNum(c))) : (i += 1) {
+                c = source[i];
+            }
+            try tokens.append(Token{ .start = start, .end = i - 1, .value = null });
+            continue;
+        }
+
+        if (isNum(c)) {
+            const start = i;
+
+            var hasDot = false;
+            var hasExp = false;
+            while (isNum(c) or (c == '.' and !hasDot) or ((c == 'e' or c == 'E') and !hasExp)) : (i += 1) {
+                if (i >= source.len) {
+                    break;
+                }
+
+                if (c == '.') {
+                    hasDot = true;
+                    continue;
+                }
+
+                if (c == 'e' or c == 'E') {
+                    hasExp = true;
+                    continue;
+                }
+            }
+            const slice = source[start..i];
+            const value = try std.fmt.parseFloat(f64, slice);
+            try tokens.append(Token{ .start = start, .end = i, .value = value });
+            continue;
+        }
+
+        // Unexpected character
+        return ScanError.UnexpectedCharacter;
+    }
+
+    return tokens;
+}
+
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -25,6 +117,14 @@ pub fn main() !void {
     try stdout.print("zigma version {d}.{d}.{d}.\n", .{ VER.major, VER.minor, VER.patch });
     try bw.flush();
     std.debug.print("stderr: initialized\n", .{});
+}
+
+test "scan empty line" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var tokens = try scan("", allocator);
+    defer tokens.deinit();
+    try expectEqual(tokens.items.len, 0);
 }
 
 test "var const" {
